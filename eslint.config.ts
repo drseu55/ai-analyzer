2691e0eba16ff06784ef1b1ffa9df03a4ec4bf8e
drseu55 <alpics1337@gmail.com>
2025-09-20 17:15:52 +0300

    chore: add eslint rule to disable specific error on certain conditions

--- eslint.config.ts
+++ eslint.config.ts
@@ -25,6 +25,18 @@ const config = [
     },
     rules: {
       ...eslintPluginTs.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
   {
